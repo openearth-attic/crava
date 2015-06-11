94fcd392e82f4f6e62a9c77fab0dcb63b8f4d3b5
Fedor Baart <fedor.baart@deltares.nl>
2015-06-12 00:56:02 +0200

    fix tests

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,7 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should have an empty list of seismics in the scope', function () {
+      expect(scope.crava.seismic.length).toBe(0);
   });
 });
